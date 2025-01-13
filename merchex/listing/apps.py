bc691176de9b72016d6716b11fd127a53d25aaf3
Samuel Saben <151065640+Samuel-Saben@users.noreply.github.com>
2025-01-13 00:52:07 -0500

    maint: Add gitignore and pre-commit

--- merchex/listing/apps.py
+++ merchex/listing/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ListingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'listing'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "listing"
--- merchex/merchex/wsgi.py
+++ merchex/merchex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchex.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "merchex.settings")
 
 application = get_wsgi_application()
